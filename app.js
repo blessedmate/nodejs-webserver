de19651fc440955f2a34eaa5771e4dd09730c854
Mate <md.quintero@uniandes.edu.co>
2021-12-26 14:53:09 -0500

    handlebars implementation

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const express = require("express");
-const app = express();
+var hbs = require("hbs");
 
+const app = express();
 const port = 8080;
+
+// Handlebars
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
 // Show static content
 app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//   res.send("Welcome");
-// });
+app.get("/", (req, res) => {
+  res.render("home", {
+    name: "Blessedmate",
+    title: "Express Webapp",
+  });
+});
 
 app.get("/generic", (req, res) => {
-  res.sendFile(__dirname + "/public/generic.html");
+  res.render("generic", {
+    name: "Blessedmate",
+    title: "Express Webapp",
+  });
 });
 
 app.get("/elements", (req, res) => {
-  res.sendFile(__dirname + "/public/elements.html");
+  res.render("elements", {
+    name: "Blessedmate",
+    title: "Express Webapp",
+  });
 });
 
 // Any undefined route
